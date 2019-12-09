284a59e3896a977686f2990bc47865cfb1b0be15
FrancisBilla <francis.billa.fb@gmail.com>
2019-12-09 12:07:15 +0000

    spring boot

---
+++ src/main/java/io/turnatabl/DevelopersService/DevelopersServiceApplication.java
@@ -0,0 +1,13 @@
+package io.turnatabl.DevelopersService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turnatabl/DevelopersService/DevelopersServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.turnatabl.DevelopersService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevelopersServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
