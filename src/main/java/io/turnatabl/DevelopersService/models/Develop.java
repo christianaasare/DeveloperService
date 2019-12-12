eabbb543d80dd74544b55fa354ba188b5b0f055b
FrancisBilla <francis.billa.fb@gmail.com>
2019-12-12 14:49:40 +0000

    play with set

--- src/main/java/io/turnatabl/DevelopersService/models/Develop.java
+++ src/main/java/io/turnatabl/DevelopersService/models/Develop.java
@@ -2,7 +2,7 @@ package io.turnatabl.DevelopersService.models;
 
 public class Develop {
     private String emp_name;
-//    private int emp_id;
+    private int emp_id;
     private String emp_phone;
     private String emp_email;
     private String emp_address;
@@ -19,9 +19,9 @@ public class Develop {
     public void setEmp_name(String emp_name) {this.emp_name = emp_name;
     }
 
-//    public int getID() {
-//        return emp_id;
-//    }
+    public int getEmp_id() {
+        return emp_id;
+    }
 //
 //    public void setID(int id) {this.emp_id = id;
 //    }
