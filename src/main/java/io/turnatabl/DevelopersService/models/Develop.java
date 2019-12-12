c4c8b859bb1dfe4f0c75f9bd655e58828b73434d
FrancisBilla <francis.billa.fb@gmail.com>
2019-12-12 14:35:02 +0000

    refactor 2

--- src/main/java/io/turnatabl/DevelopersService/models/Develop.java
+++ src/main/java/io/turnatabl/DevelopersService/models/Develop.java
@@ -1,62 +1,62 @@
 package io.turnatabl.DevelopersService.models;
 
 public class Develop {
-    private String name;
-    private int id;
-    private String phone;
-    private String email;
-    private String address;
+    private String emp_name;
+    private int emp_id;
+    private String emp_phone;
+    private String emp_email;
+    private String emp_address;
 
 
     public Develop(){
 
     }
 
-    public String getName() {
-        return name;
+    public String getEmp_name() {
+        return emp_name;
     }
 
-    public void setName(String name) {this.name = name;
+    public void setEmp_name(String emp_name) {this.emp_name = emp_name;
     }
 
     public int getID() {
-        return id;
+        return emp_id;
     }
 
-    public void setID(int id) {this.id = id;
+    public void setID(int id) {this.emp_id = id;
     }
 
 
 
-    public String getPhone() {
-        return phone;
+    public String getEmp_phone() {
+        return emp_phone;
     }
 
-    public void setPhone(String phone) { this.phone = phone;
+    public void setEmp_phone(String emp_phone) { this.emp_phone = emp_phone;
     }
 
-    public String getEmail() {
-        return email;
+    public String getEmp_email() {
+        return emp_email;
     }
 
-    public void setEmail(String email) {this.email = email;
+    public void setEmp_email(String emp_email) {this.emp_email = emp_email;
     }
 
-    public String getAddress() {
-        return address;
+    public String getEmp_address() {
+        return emp_address;
     }
 
-    public void setAddress(String address) {this.address = address;
+    public void setEmp_address(String emp_address) {this.emp_address = emp_address;
     }
 
     @Override
     public String toString() {
         return "Develop{" +
-                "dev_name='" + name + '\'' +
-                ", developer_id=" + id +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
+                "dev_name='" + emp_name + '\'' +
+                ", developer_id=" + emp_id +
+                ", phone='" + emp_phone + '\'' +
+                ", email='" + emp_email + '\'' +
+                ", address='" + emp_address + '\'' +
                 '}';
     }
 }
